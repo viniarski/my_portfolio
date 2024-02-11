1fc39565fd84f2d2fb78e903bc9bb373ca683451
Michal Winiarski <41969647+viniarski@users.noreply.github.com>
2024-02-11 14:08:04 +0000

    update

--- app.js
+++
@@ -1,8 +0,0 @@
-import { SpeedInsights } from '@vercel/speed-insights/next';
-
-function toggleMenu() {
-  const menu = document.querySelector('.menu-links');
-  const icon = document.querySelector('.hamburger-icon');
-  menu.classList.toggle('open');
-  icon.classList.toggle('open');
-}
